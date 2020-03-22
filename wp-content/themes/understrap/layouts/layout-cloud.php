d11013b094610cbe501cb18a9c3035407b11de46
Ivi Hysenbelli <ihysenbelli@kuf.com>
2020-03-22 17:48:31 +0100

    Two Columns Row

--- wp-content/themes/understrap/layouts/layout-cloud.php
+++ wp-content/themes/understrap/layouts/layout-cloud.php
@@ -6,6 +6,9 @@ if( have_rows('layouts') ):
 		if( get_row_layout() == 'section_main_banner' ):
 			get_template_part( 'sections/section', 'main-banner' );
 		endif;
+		if( get_row_layout() == 'section_two_columns' ):
+			get_template_part( 'sections/section', 'two-columns' );
+		endif;
 		
 	endwhile;
 endif;
---
+++ wp-content/themes/understrap/sections/section-two-columns.php
@@ -0,0 +1,20 @@
+<?php $background = get_sub_field_object('background_color'); ?>
+<section class="section section-two-columns <?php if($background['value'] == true) { echo "has-bg"; } ?>">
+	<?php $order = get_sub_field('order'); ?>
+	<div class="row <?php if ($order == "rtl") { echo "flex-row-reverse"; } ?>">
+		<div class="col-md-12 col-lg-6 image-col">
+			<?php $image = get_sub_field('image') ?>
+			<img src="<?php echo$image['url']; ?>" alt="">
+		</div>
+		<div class="col-md-12 col-lg-6 data-col">
+			<div class="data">
+				<div class="title">
+					<h2><?php the_sub_field('title'); ?></h2>
+				</div>
+				<div class="description">
+					<p><?php the_sub_field('editor'); ?></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
