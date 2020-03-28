67631a29e4475eccbb83606ba231ea3edb8efce0
Ivi Hysenbelli <iv.hysenbelli@gmail.com>
2020-03-28 18:23:25 +0100

    UI Fixes

--- wp-content/themes/understrap/sections/section-text.php
+++ wp-content/themes/understrap/sections/section-text.php
@@ -1,16 +1,18 @@
 <section class="section section-text">
 <div class="container">
   	<div class="row">
-      <div class="title">
-        <h2><?php the_sub_field('section_title') ?></h2>
-      </div>
-      <div class="subtitle">
-          <?php if(!empty(get_sub_field('editor'))): ?>
-            <p class="header-description">
-              <?php the_sub_field('editor'); ?>
-            </p>
-        <?php endif; ?>
-      </div>
-  	</div>
+  		<div class="col-12">
+			<div class="title">
+		        <h2><?php the_sub_field('section_title') ?></h2>
+		      </div>
+		      <div class="subtitle">
+		          <?php if(!empty(get_sub_field('editor'))): ?>
+		            <p class="header-description">
+		              <?php the_sub_field('editor'); ?>
+		            </p>
+		        <?php endif; ?>
+		      </div>
+		  	</div>
+  		</div>
   </div>  
 </section>
