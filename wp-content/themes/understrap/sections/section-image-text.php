95cdca11b84df8ed2ba8f86ad501cb219c077606
Ivi Hysenbelli <iv.hysenbelli@gmail.com>
2020-03-28 17:42:56 +0100

    Section Text

--- wp-content/themes/understrap/sections/section-image-text.php
+++ wp-content/themes/understrap/sections/section-image-text.php
@@ -3,7 +3,9 @@
     <div class="row">
       <div class="title">
         <h2><?php the_sub_field('section_title') ?></h2>
-        <?php if(!empty(get_sub_field('section_description'))): ?>
+      </div>
+      <div class="subtitle">
+          <?php if(!empty(get_sub_field('section_description'))): ?>
             <p class="header-description">
               <?php the_sub_field('section_description'); ?>
             </p>
@@ -14,7 +16,7 @@
   <div class="container">
     <div class="row">
       <?php while(have_rows('informations')): the_row(); ?>
-        <div class="col-lg-4">
+        <div class="col-sm-12 col-md-6 col-lg-4 single-col">
           <div class="data">
             <?php $image = get_sub_field('image'); ?>
             <img src="<?php echo $image['url']; ?>" alt="">
--- wp-content/themes/understrap/sections/section-videos.php
+++ wp-content/themes/understrap/sections/section-videos.php
@@ -1,11 +1,11 @@
 <section class="section section-videos">
   <div class="container">
-    <?php $count = count(get_sub_field(videos)); ?>
+    <?php $count = count(get_sub_field('videos')); ?>
       <div class="row">
         <?php while(have_rows('videos')) : the_row(); ?>
-          <div class="col-lg-<?php echo 12/$count; ?>">
+          <div class="col-lg-<?php echo 12/$count; ?> single-col">
             <div class="title"><?php the_sub_field('video_title'); ?></div>
-            <a href="#">
+            <a href="<?php the_sub_field('video_url') ?>">
               <?php $img = get_sub_field('video_thumbnail'); ?>
               <img src="<?php echo $img['url']; ?>" alt="">
             </a>
