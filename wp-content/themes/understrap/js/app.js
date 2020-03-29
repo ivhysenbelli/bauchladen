de9b5e6c4d4efd5e323b17d36386a62207a7ebee
Ivi Hysenbelli <ihysenbelli@kuf.com>
2020-03-30 00:15:24 +0200

    Added Termine Section

--- wp-content/themes/understrap/js/app.js
+++ wp-content/themes/understrap/js/app.js
@@ -9,4 +9,110 @@ jQuery(document).ready(function($) {
             $("#wrapper-navbar").removeClass("scrolled");
         }
     });
+
+    
+    $(".custom-checkbox").click(function(event) {
+        if (this.checked) {
+
+            $('.custom-checkbox#' + this.id).prop("checked", true);
+
+            var ul = document.getElementById("kurse-list");
+            var li = document.createElement('li');
+            var id = String(this.value)
+            id = id.replace(/\//g, "-");
+            li.setAttribute('id', id);
+            var appendValue = document.createTextNode(this.name);
+            li.appendChild(appendValue);
+            ul.appendChild(li);
+
+        } else {
+
+            $('.custom-checkbox#' + this.id).prop("checked", false);
+            id = String(this.value)
+            id = id.replace(/\//g, "-");
+            $('li#' + id).remove();
+        }
+    });
+
+    $('.wpcf7-submit').on('click', function(e) {
+
+        var satatement = '';
+
+        document.querySelectorAll('ul#kurse-list li').forEach(function(item) {
+            satatement += item.textContent.trim();
+        });
+        var box = $("textarea#kurs-list-id-form");
+        box.val(box.val() + satatement);
+
+    });
+
+    $('.btn-toggle').on('click', function(e) {
+        $('.custom-contact-form').toggleClass("hidden");
+        if (!$('.custom-contact-form').hasClass('hidden')) {
+            $('.btn-toggle').text("Schließen").addClass('close');
+        } else {
+            $('.btn-toggle').text("Anfragen").removeClass('close');
+        }
+    });
+
+    //Mobile Scripts
+
+    if ($(window).width() < 568.99) {
+        var checkedNr = 0;
+        $(".custom-checkbox").click(function(event) {
+            if (this.checked) {
+                checkedNr++;
+            } else {
+                checkedNr--;
+            }
+
+            if (checkedNr >= 1 && !isOnScreen($('.custom-contact-form'))) {
+                $('.details-row.details-footer').addClass('show');
+            } else {
+
+                $('.details-row.details-footer').removeClass('show');
+            }
+
+            $(window).scroll(function() {
+                if (!isOnScreen($('.custom-contact-form')) && checkedNr >= 1) {
+                    $('.details-row.details-footer').addClass('show');
+                } else {
+                    $('.details-row.details-footer').removeClass('show');
+                }
+            });
+
+            function isOnScreen(elem) {
+                // if the element doesn't exist, abort
+                if (elem.length == 0) {
+                    return;
+                }
+                var $window = jQuery(window)
+                var viewport_top = $window.scrollTop()
+                var viewport_height = $window.height()
+                var viewport_bottom = viewport_top + viewport_height
+                var $elem = jQuery(elem)
+                var top = $elem.offset().top
+                var height = $elem.height()
+                var bottom = top + height
+
+                return (top >= viewport_top && top < viewport_bottom) ||
+                    (bottom > viewport_top && bottom <= viewport_bottom) ||
+                    (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+            }
+
+        });
+
+        $('.footer-hook-col .btn-toggle').click(function() {
+            if (($(this).hasClass('close'))) {
+                $('html, body').animate({
+                    scrollTop: $(".anfragen-form").offset().top
+                }, 800);
+            }
+        });
+    }
+
+    $('.datepicker-here').flatpickr({
+    	'locale': 'de',
+    });
+
 });
--- wp-content/themes/understrap/sections/section-termine.php
+++ wp-content/themes/understrap/sections/section-termine.php
@@ -1,3 +1,119 @@
+<?php 
+$args = array(
+		'post_type' 		=> 'event',
+		'posts_per_page' 	=> -1,
+		'order'				=> 'ASC',
+	);
+
+$posts = get_posts($args);
+?>
 <section class="section section-termine">
-  
+	<div class="container">
+	  	<div class="row">
+	  		<div class="col-12">
+				<div class="title">
+			        <h2><?php the_sub_field('section_title') ?></h2>
+			      </div>
+			      <div class="subtitle">
+			          <?php if(!empty(get_sub_field('editor'))): ?>
+			            <p class="header-description">
+			              <?php the_sub_field('editor'); ?>
+			            </p>
+			        <?php endif; ?>
+			      </div>
+		  	</div>
+		  	<div class="col-12">
+		  		<nav>
+				  <div class="nav nav-tabs" id="nav-tab" role="tablist">
+				    <?php $flag = 0; ?>
+					<?php foreach ($posts as $post ): ?>
+						<a class="nav-item nav-link" id="nav-<?= $post->ID ?>-tab" data-toggle="tab" href="#nav-<?= $post->ID ?>" role="tab" aria-controls="nav-<?= $post->ID ?>" aria-selected="false"><?= $post->post_title; ?></a>
+						<?php $flag = 1; ?>
+					<?php endforeach; ?>
+				  </div>
+				</nav>
+				<div class="tab-content" id="nav-tabContent">
+				  <?php $flag2 = 0; ?>
+					<?php foreach ($posts as $post ): ?>
+				  <div class="tab-pane fade <?php if ($flag2 == 0){ echo "show active"; } ?>" id="nav-<?= $post->ID ?>" role="tabpanel" aria-labelledby="nav-home-tab">
+							<div class="single-post-data">
+								<?php if(have_rows('kurs',$post->ID)): ?>
+								<table class="date_table_db custom-table">
+									<tr class="table_head_row">
+										<th>Datum</th>
+										<th>Zeit</th>
+										<th>Kurs</th>
+										<th>Module</th>
+										<th>Auswählen</th>
+									</tr>
+									<?php $idVal = 1; ?>
+								<?php while(have_rows('kurs',$post->ID)): the_row(); ?>
+									<tr class="custom-table-row">
+										<td class="date">
+											<?php 
+											setlocale(LC_TIME, "de_DE");
+											$time = strtotime(str_replace('/', '-', get_sub_field('course_date')));
+											$date = date('d.m.Y', strtotime(str_replace('/', '-', get_sub_field('course_date'))));
+											$newformat = strftime('%a, ',$time);
+											echo $newformat.$date;
+											?>
+										</td>
+										<td class="time">
+											<?php the_sub_field('course_timing'); ?>
+										</td>
+										<?php $kurs = get_sub_field_object('course_title') ?>
+										<td class="kurs">
+											<?php echo $kurs['choices'][$kurs['value']] ?>
+										</td>
+										<td class="module">
+											<?php echo $kurs['value']; ?>
+										</td>
+										<td class="desktop-checkbox check-kurs">
+											<div class="check-container">
+												<input type="checkbox" id="<?php echo strtolower($post->post_title)."-id-".$idVal ?>" class="custom-checkbox" value="<?= the_sub_field('course_date'); ?>" name="<?php echo $newformat.$date; ?> | <?php the_sub_field('course_timing')?> | <?= $kurs['choices'][$kurs['value']] ?> | <?= $kurs['value'] ?> ">
+												<span class="checkmark"></span>
+											</div>
+										</td>
+									</tr>
+									<?php $idVal++; ?>
+								<?php endwhile; ?>
+								</table>
+							<?php else: ?>
+								<h4>Comming Soon..</h4>
+							<?php endif; ?>
+							</div>
+
+				  </div>
+				<?php $flag2 = 1; ?>
+					<?php endforeach; ?>
+				</div>
+		  	</div>
+  		</div>
+  		<div class="row">
+  			<div class="col-12">
+  				<div class="custom-contact-form hidden">
+					<?php 
+						$contactFormID = get_field('contact_form_id','options');
+						$contactFormName = get_field('contact_form_name','options');
+
+						echo do_shortcode( '[contact-form-7 id="'.$contactFormID.'" title="'.$contactFormName.'"]' );
+					?>
+				</div>
+  			</div>
+  		</div>
+  		<div class="row">
+  			<div class="col-12">
+  				<div class="details-row main-details">
+				<div class="single-col descripton-col">
+					<p class="description">
+						<?php wpautop(the_field('description','options')); ?>
+					</p>
+				</div>
+				<div class="single-col button-col">
+					<span class="btn-toggle"> <?php the_field('button_title','options') ?> </span> 
+				</div>
+			</div>
+  			</div>
+  		</div>
 </section>
+<?php wp_reset_postdata(); ?>
