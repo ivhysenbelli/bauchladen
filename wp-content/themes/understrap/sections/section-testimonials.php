ea5a1905545e1226e17b7f7d2cf615adb05d5c88
Ivi Hysenbelli <ihysenbelli@kuf.com>
2020-03-29 23:46:10 +0200

    Added testimonials

--- wp-content/themes/understrap/sections/section-testimonials.php
+++ wp-content/themes/understrap/sections/section-testimonials.php
@@ -1,3 +1,24 @@
 <section class="section section-testimonials">
-  
+<div class="container">
+  	<div class="row">
+  		<div class="col-12">
+			<div class="title">
+		        <h2><?php the_sub_field('section_title') ?></h2>
+		      </div>
+		      <div class="subtitle">
+		          <?php if(!empty(get_sub_field('editor'))): ?>
+		            <p class="header-description">
+		              <?php the_sub_field('editor'); ?>
+		            </p>
+		        <?php endif; ?>
+		      </div>
+		  	</div>
+		  	<div class="col-12">
+		  		<div class="testimonials">
+			  		<?php $action = get_sub_field('testimonials_shortcode'); ?>
+			  		<?php echo do_shortcode($action); ?>
+		  		</div>
+		  	</div>
+  		</div>
+  </div>  
 </section>
