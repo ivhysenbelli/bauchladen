9a9a94a1e118fa3632a311e1ca4178310e2bce66
Ivi Hysenbelli <ihysenbelli@kuf.com>
2020-04-01 18:31:00 +0200

    Links

--- wp-content/themes/understrap/sections/section-links.php
+++ wp-content/themes/understrap/sections/section-links.php
@@ -1,3 +1,16 @@
-<section class="section section-links">
-
+<section id="<?php strtolower(the_sub_field('section_id')); ?>" class="section section-links">
+	<div class="container">
+		<div class="row">
+			<div class="col-12">
+				<div class="page-title">
+					<h2><?php the_sub_field('section_title'); ?></h2>
+				</div>
+			</div>
+			<?php while(have_rows('links')): the_row(); ?>
+				<div class="col-lg-4">
+					<?php the_sub_field('link'); ?>
+				</div>
+			<?php endwhile;?>
+		</div>
+	</div>
 </section>
