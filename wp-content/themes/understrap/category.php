37159e1d3b2b4b8a652babf58cfbdd40abaa178e
Ivi Hysenbelli <ihysenbelli@kuf.com>
2020-04-03 12:17:58 +0200

    category-page

---
+++ wp-content/themes/understrap/category.php
@@ -0,0 +1,24 @@
+<?php get_header(); ?>
+<div class="container-fluid">
+	<div class="row">
+		<div class="col-lg-9 posts-col">
+			
+			
+		</div>
+		<div class="col-lg-3 sidebar-col">
+			<div class="sidebar-navigation">
+				
+			</div>
+			<div class="social-links">
+				
+			</div>
+			<div class="events-dates">
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+
+
+<?php get_footer(); ?>
